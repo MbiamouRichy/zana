8818848013d8aa60fd409d9f56c02e24b741a093
Mbiamou Richy <richymbiamou52@gmail.com>
2026-04-06 00:44:38 +0100

    update verify

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -50,30 +50,5 @@ export default async function page(props: {
         </>
       )}
     </main>
-  const searchParams = await props.searchParams;
-  const email = searchParams.email;
-  return (
-    <div className="min-h-screen w-screen  flex-col flex items-center justify-center p-2">
-      <h1>Vérification de l{`'`}e-mail</h1>
-      <Card className="w-full max-w-prose">
-        <CardHeader>
-          <CardTitle>Veuillez vérifier votre adresse e-mail.</CardTitle>
-
-          {email ? (
-            <CardDescription>
-              Un e-mail de vérification a été envoyé à{" "}
-              <a
-                className="text-primary hover:underline"
-                href={`mailto:${email}`}
-              >
-                {email}
-              </a>
-              .Rendez vous dans votre boîte de réception d{`'`}e-mail et cliquer
-              sur le lien de vérification contenu dans cet e-mail.
-            </CardDescription>
-          ) : null}
-        </CardHeader>
-      </Card>
-    </div>
   );
 }
